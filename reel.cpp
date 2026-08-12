1d631e016897914664a4b2821badfd5dcac414cf
MD BELAL ANSARI <mdbelalansari7865@gmail.com>
2026-08-12 15:28:48 +0530

    reel code updated

--- reel.cpp
+++ reel.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello";
+    int n;
+    cout<<"Enter no :";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<"hello"<<endl;
+    }
 }
